69c04904d8c6b475e3399ac6a85296ac33c2699c
heewon <luke@akaon.com>
2018-02-21 16:18:41 +0900

    add 04Project mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@sum
+D=M
+@i
+M=1
+(LOOP)
+@R0
+D=M
+@i
+D=M-D
+@END
+D;JGT
+@sum
+D=M
+@R1
+D=D+M
+@i
+D=M
+D=D+M
+@LOOP
+0;JMP
+(END)
+@SUM
+D=M
+@R2
+M=D
+
